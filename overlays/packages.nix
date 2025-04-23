08d4a6d3d459a6ae2fb65bd267dd49af23a60fe9
postsolar <120750161+postsolar@users.noreply.github.com>
2025-04-23 10:41:13 +0300

    2025-04-23 10:41:13
    Override copyq due to build failure
    
    The version of `copyq` in the default channel (likely version 9.1.0) is currently failing to build.
    
    This commit overrides the package definition to pull `copyq` from the `nixpkgs-master` input, where the build issue has been resolved.
    
    Reference: https://nixpk.gs/pr-tracker.html?pr=398926

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -19,12 +19,12 @@ inputs: f: p:
       })
       ;
 
-  # # build failure on 9.1.0, so use an updated version
-  # # https://nixpk.gs/pr-tracker.html?pr=398926
-  # copyq =
-  #   inputs.nixpkgs-master.legacyPackages.${f.system}.copyq;
+  # build failure on 9.1.0
+  # https://nixpk.gs/pr-tracker.html?pr=398926
+  copyq =
+    inputs.nixpkgs-master.legacyPackages.${f.system}.copyq;
 
-  # build failure, so use an updated version
+  # build failure
   # https://nixpk.gs/pr-tracker.html?pr=399990
   streamrip =
     inputs.nixpkgs-master.legacyPackages.${f.system}.streamrip;
