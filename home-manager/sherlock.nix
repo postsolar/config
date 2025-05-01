0e9dc2ed73be05a56d38a3d84814849ae1105778
postsolar <120750161+postsolar@users.noreply.github.com>
2025-05-02 02:16:33 +0300

    R 02 mai 2025 02:16:33
    Add comments explaining sherlock configuration.
    
    Include comments that:
    - Document the source of the sherlock package (from an input).
    - Mention the potential for sherlock to be added to nixpkgs in the near future, referencing the relevant pull request.
    - Discuss the existence of a home-manager module for sherlock in the project's repository and the decision not to use it.

--- home-manager/sherlock.nix
+++ home-manager/sherlock.nix
@@ -1,8 +1,12 @@
 { inputs, system, ... }:
 
 {
+  # TODO: will soon be in nixpkgs, see https://github.com/nixOS/nixpkgs/pull/403004
   home.packages = [
     inputs.sherlock.packages.${system}.default
   ];
+
+  # there's a home-manager module in https://github.com/Skxxtz/sherlock/blob/main/nix/home-manager.nix
+  # but it's kinda goofy, i'd just write my files directly here
 }
 
